f9a325464873e2a4975b5f6909ac8096be9bafc9
Chris Laughlin <chrislfla@gmail.com>
2024-01-21 23:52:30 -0500

    Fetching data through redux and making code cleaner and easier to read

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,40 +1,24 @@
-import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import { PRODUCTS_URL } from '../constants.js'; // Import the constant
+import { useGetProductsQuery } from '../slices/productsApiSlice.js';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(PRODUCTS_URL);
-
-        if (!response.ok) {
-          throw new Error(`Request failed with status ${response.status}`);
-        }
-
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.error('Error fetching products:', error.message);
-      }
-    };
-
-    fetchProducts();
-  }, []);
 
   return (
     <>
+      { isLoading ? (<h2>Loading...</h2>) : error ? (<div>{error?.data.message || error.error}</div>) : (<>
       <h1>Latest Products</h1>
       <Row>
-        {products && products.map((product) => (
+        {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
-      </Row>
+      </Row></>)}
+
+      
     </>
   );
 };
--- frontend/src/screens/ProductSceen.jsx
+++ frontend/src/screens/ProductSceen.jsx
@@ -1,82 +1,66 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating'
-import { PROXY } from '../constants.js'; // Import the constant
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 
 const ProductSceen = () => {
-  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
 
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const response = await fetch(`${PROXY}/api/products/${productId}`);
-
-        if (!response.ok) {
-          throw new Error(`Request failed with status ${response.status}`);
-        }
-
-        const data = await response.json();
-        setProduct(data);
-      } catch (error) {
-        console.error('Error fetching product:', error.message);
-      }
-    };
-
-    fetchProduct();
-  }, [productId]);
+  const {data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
   return <>
     <Link className="btn btn-light my-3" to="/">Go back</Link>
 
+    {isLoading ? (<h2>Loading...</h2>) : error ? (<div>{error?.data?.message || error.error}</div>) : (<>
     <Row>
-      <Col md={5}>
-        <Image src={product.image} alt={product.name} fluid />
-      </Col>
-      <Col md={4}>
-        <ListGroup>
+    <Col md={5}>
+      <Image src={product.image} alt={product.name} fluid />
+    </Col>
+    <Col md={4}>
+      <ListGroup>
+        <ListGroup.Item>
+          <h3>{product.name}</h3>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        </ListGroup.Item>
+        <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+        <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+      </ListGroup>
+    </Col>
+    <Col md={3}>
+      <Card>
+        <ListGroup variant="flush">
           <ListGroup.Item>
-            <h3>{product.name}</h3>
+            <Row>
+              <Col>Price: </Col>
+              <Col>
+                <strong>${product.price}</strong>
+              </Col>
+            </Row>
           </ListGroup.Item>
           <ListGroup.Item>
-            <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+            <Row>
+              <Col>Status: </Col>
+              <Col>
+                <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+              </Col>
+            </Row>
           </ListGroup.Item>
-          <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-          <ListGroup.Item>Description: {product.description}</ListGroup.Item>
-        </ListGroup>
-      </Col>
-      <Col md={3}>
-        <Card>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <Row>
-                <Col>Price: </Col>
-                <Col>
-                  <strong>${product.price}</strong>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Row>
-                <Col>Status: </Col>
-                <Col>
-                  <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
-                </Col>
-              </Row>
-            </ListGroup.Item>
 
-            <ListGroup.Item>
-              <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
-                Add to Cart
-              </Button>
-            </ListGroup.Item>
-          </ListGroup>
-        </Card>
-      </Col>
-    </Row>
+          <ListGroup.Item>
+            <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+              Add to Cart
+            </Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+    </Col>
+  </Row>
+    </>) }
+    
   </>;
 }
 

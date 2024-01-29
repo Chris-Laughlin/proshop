7451a88bcf9f3f23fdfddce72a9db0e437570951
Chris Laughlin <chrislfla@gmail.com>
2024-01-28 21:16:35 -0500

    Implements everything to do with the cart screen. Users are now able to add items to their cart and it will show their sub total when in the cartScreen. Users can also add more items to their cart while viewing their cart. If a user wishes to delete an item from their cart they can do so by clicking on the trash can button right next to the item.

--- frontend/src/screens/ProductSceen.jsx
+++ frontend/src/screens/ProductSceen.jsx
@@ -1,17 +1,29 @@
-import { useParams } from "react-router-dom";
+import {useState} from 'react';
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
 import { Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Form, Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating'
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import { addToCart } from '../slices/cartSlice';
 
 
 const ProductSceen = () => {
   const { id: productId } = useParams();
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [qty, setQty] = useState(1);
+
   const {data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
+  const addToCartHandler = () => {
+    dispatch(addToCart({...product, qty}));
+    navigate('/cart');
+  }
   return <>
     <Link className="btn btn-light my-3" to="/">Go back</Link>
   
@@ -54,8 +66,29 @@ const ProductSceen = () => {
             </Row>
           </ListGroup.Item>
 
+          {product.countInStock>0 &&(
+            <ListGroup.Item>
+              <Row>
+                <Col>Qty</Col>
+                <Col>
+                  <Form.Control as='select'
+                    value={qty}
+                    onChange={ (e) => setQty(Number(e.target.value))}>
+                      {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x+1} value={x+1}>
+                          { x + 1}
+                        </option>
+                      )) }
+                    </Form.Control> 
+                    
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          )}
+
           <ListGroup.Item>
-            <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+            <Button className="btn-block" type="button" disabled={product.countInStock === 0} 
+            onClick={addToCartHandler}>
               Add to Cart
             </Button>
           </ListGroup.Item>

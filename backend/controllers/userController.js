badaa7337d08446f5ef6c458b9644152b410dffe
Chris Laughlin <chrislfla@gmail.com>
2024-01-28 22:32:47 -0500

    Prepping for adding a login screen and authenticating users

---
+++ backend/controllers/userController.js
@@ -0,0 +1,77 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+// @desc    Auth user & get token
+// @route    Post /api/users/login
+// @access    Public
+const authUser = asyncHandler(async (req, res) => {
+    res.send('auth user');
+});
+
+// @desc    Register User
+// @route    POST /api/users
+// @access    Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.send('register user');
+});
+
+// @desc    Logout user /clear cookie
+// @route    POST /api/users/logout
+// @access   Private
+const logoutUser = asyncHandler(async (req, res) => {
+    res.send('logout user');
+});
+
+// @desc    Get User profile
+// @route    GET /api/users/profile
+// @access    Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.send('get user profile');
+});
+
+// @desc    Update user profile
+// @route    PUT /api/users/profile
+// @access    Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.send('update user profile');
+});
+
+// @desc    Get users
+// @route    GET /api/users/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+    res.send('get users');
+});
+
+// @desc    Get user by id
+// @route    GET /api/users/:id
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+    res.send('get user by id');
+});
+
+// @desc    Delete users
+// @route   DELETE /api/users/users
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+    res.send('delete users');
+});
+
+// @desc    Update users
+// @route   PUT /api/users/users
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+    res.send('Update users');
+});
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser
+}

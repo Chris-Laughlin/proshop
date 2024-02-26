c4d4f07c84e7fdaef13c3252a4c4ebcfc9c51c96
Chris Laughlin <chrislfla@gmail.com>
2024-02-25 19:14:39 -0500

    There is now product pagination with 4 products on each page and users and admin can switch between pages

--- frontend/src/screens/Admin/ProductListScreen.jsx
+++ frontend/src/screens/Admin/ProductListScreen.jsx
@@ -1,14 +1,18 @@
 import { LinkContainer } from 'react-router-bootstrap'
 import { Table, Button, Row, Col } from 'react-bootstrap'
 import { FaEdit, FaTrash } from 'react-icons/fa'
+import { useParams } from 'react-router-dom'
 import Message from '../../components/Message'
+import Paginate from '../../components/Paginate'
 import Loader from '../../components/Loader'
 import { useGetProductsQuery, useCreateProductMutation,
         useDeleteProductMutation } from '../../slices/productsApiSlice'
 import {toast } from 'react-toastify'
 
 const ProductListScreen = () => {
-    const { data: products, error, isLoading, refetch } = useGetProductsQuery();
+
+    const {pageNumber} = useParams();
+    const { data, error, isLoading, refetch } = useGetProductsQuery({pageNumber});
 
    
 
@@ -64,7 +68,7 @@ const ProductListScreen = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {products.map((product) => (
+                    {data.products.map((product) => (
                         <tr key={product._id}>
                             <td>{product._id}</td>
                             <td>{product.name}</td>
@@ -85,6 +89,7 @@ const ProductListScreen = () => {
                     ))}
                 </tbody>
             </Table>
+            <Paginate pages={data.pages} page={data.page} isAdmin={true} />
         </>
     )}
   </>

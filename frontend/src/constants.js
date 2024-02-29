8a74b00e84d4a9645c21c0b5dd09d5422e64414b
Chris Laughlin <chrislfla@gmail.com>
2024-02-29 15:09:48 -0500

    added proxy again

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 export const PROXY = "http://localhost:5000";
-export const PRODUCTS_URL = "/api/products";
+export const PRODUCTS_URL = PROXY + "/api/products";
 export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : PROXY;
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
-export const UPLOADS_URL = '/api/upload';
+export const USERS_URL = PROXY +'/api/users';
+export const ORDERS_URL = PROXY +'/api/orders';
+export const PAYPAL_URL = PROXY +'/api/config/paypal';
+export const UPLOADS_URL = PROXY +'/api/upload';

8f892e9da070f7db3c772dc6b0193e1d6cda12ba
Chris Laughlin <chrislfla@gmail.com>
2024-02-29 12:16:56 -0500

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ import uploadRoutes from './routes/uploadRoutes.js';
 const corsOptions = {
     origin: true, // Change this to the origin(s) you want to allow.
     credentials: true, // Indicates that cookies and credentials should be included.
-  };
+};
 
 const port = process.env.PORT || 5000;
 
@@ -40,8 +40,22 @@ app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send({clientId: process.env.PAYPAL_CLIENT_ID}));
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
 
 
 app.use(notFound);

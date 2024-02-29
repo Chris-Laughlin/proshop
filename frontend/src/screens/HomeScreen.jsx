4f913e5683cececba31e22d3358f41292e8f727e
Chris Laughlin <chrislfla@gmail.com>
2024-02-28 21:19:46 -0500

    Allow user to search for products using the search box at the top of the screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader.jsx';
 import Message from '../components/Message.jsx';
@@ -7,12 +7,13 @@ import Paginate from '../components/Paginate';
 import { useGetProductsQuery } from '../slices/productsApiSlice.js';
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
-  const { data, isLoading, error } = useGetProductsQuery({pageNumber});
+  const { pageNumber, keyword } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({keyword, pageNumber});
 
 
   return (
     <>
+    { keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link>}
       { isLoading ? (<Loader/>) : error ? 
       (<Message variant = 'danger'>{error?.data.message || error.error}</Message>) 
       : (<>
@@ -24,7 +25,7 @@ const HomeScreen = () => {
           </Col>
         ))}
       </Row>
-      <Paginate pages={data.pages} page={data.page} />
+      <Paginate pages={data.pages} page={data.page} keyword={keyword ? keyword : ''} />
       </>)}
     </>
   );
